338a1faff98e2b58512f0463cefe39a9bed7f4ce
Aununo <fikavecgold@gmail.com>
2025-11-01 20:05:05 +0800

    update

--- CTF/cnss/2025/🌟栖居于可能性之中 (baby).py
+++
@@ -1,9 +0,0 @@
-import base64
-
-a = 'MNZWK327L5SW6ZDGOJ2GQZLPMNPWS5LOPNWG25DUL5ZF6X3ZN5XV65LBNJXHG43XMNSW62DXNRXWG4C7OB4V63TPL56Q===='
-
-print(base64.b32decode(a).decode())
-
-# cseo__eodfrtheoc_iun{lmtt_r__yoo_uajnsswceohwlocp_py_no_}
-
-# cnss{welcome_to_the_world_of_crypto_hope_you_can_join_us}
